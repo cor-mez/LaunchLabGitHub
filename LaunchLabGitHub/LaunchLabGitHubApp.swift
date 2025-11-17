b58b5bce3695eeece6b9c9ce2e16bef00924ab50
cor-mez <cormeza@gmail.com>
2025-11-16 22:26:08 -0800

    Initial Commit

---
+++ LaunchLabGitHub/LaunchLabGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchLabGitHubApp.swift
+//  LaunchLabGitHub
+//
+//  Created by Cory Meza on 11/16/25.
+//
+
+import SwiftUI
+
+@main
+struct LaunchLabGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LaunchLabGitHubTests/LaunchLabGitHubTests.swift
@@ -0,0 +1,16 @@
+//
+//  LaunchLabGitHubTests.swift
+//  LaunchLabGitHubTests
+//
+//  Created by Cory Meza on 11/16/25.
+//
+
+import Testing
+
+struct LaunchLabGitHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
